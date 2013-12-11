71139f3f3d4de7580d94fc76001b0413c2cbcc31
Joseph Hughes <joshughes@paypal.com>
2013-12-11 09:42:08 -0500

    Require the rest of the files needed for backport

--- lib/rails/patch/json_encoder_refactor_backport.rb
+++ lib/rails/patch/json_encoder_refactor_backport.rb
@@ -1,4 +1,7 @@
 require "rails/patch/json_encoder_refactor_backport/version"
+require "rails/patch/json_encoder_refactor_backport/active_support/core_ext/object/json"
+require "rails/patch/json_encoder_refactor_backport/active_support/json/decoding"
+require "rails/patch/json_encoder_refactor_backport/active_support/json/encoding"
 
 module Rails
   module Patch
